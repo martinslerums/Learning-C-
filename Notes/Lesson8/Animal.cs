1f7dc26b9f8c989ab8f74925f6c656211900b811
martleru <68188509+martleru@users.noreply.github.com>
2024-07-03 10:44:16 +0300

    Eight lesson

---
+++ Notes/Lesson8/Animal.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace LessonEight
+{
+  class Animal
+  {
+    public string Name { get; set; }
+    public Animal(string name = "No Name")
+    {
+      Name = name;
+    }
+
+  }
+}
