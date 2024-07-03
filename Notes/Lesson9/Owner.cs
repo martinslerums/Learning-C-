87163284f30118cf47afb05929c96b2e876a4d62
martleru <68188509+martleru@users.noreply.github.com>
2024-07-03 15:50:03 +0300

    Ninth lesson

---
+++ Notes/Lesson9/Owner.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonNine
+{
+  class Owner
+  {
+    public string? Name { get; set; }
+    public int OwnerID { get; set; }
+  }
+}
